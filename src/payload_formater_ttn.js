4e62337890b28d7a0857099c8422ae7685746393
gregoiremahon <greg.mahondu78@gmail.com>
2023-09-27 13:44:14 +0200

    Modification payload formater pour capteurs températures

--- src/payload_formater_ttn.js
+++ src/payload_formater_ttn.js
@@ -2,7 +2,8 @@ function decodeUplink(input) {
     var data = {};
     data.temperatureDHT11 = (input.bytes[1] << 8 | (input.bytes[0]));
     data.humidityDHT11 = (input.bytes[3] << 8 | (input.bytes[2]));
-    data.temperatureDS18B20 = (input.bytes[5] << 8 | (input.bytes[4]));
+    data.temperatureDS18B20_1 = (input.bytes[5] << 8 | (input.bytes[4]));
+    data.temperatureDS18B20_2 = (input.bytes[7] << 8 | (input.bytes[6]));
     return {
       data: data,
       warnings: [],
