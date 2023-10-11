e2e2f5f7f92420c94d4d7862e8d242e7792995f6
gregoiremahon <greg.mahondu78@gmail.com>
2023-10-11 11:45:19 +0200

    Ajout mesure du poids en long et envoi via LoRa

--- src/mkrwan/mkrwan.ino
+++ src/mkrwan/mkrwan.ino
@@ -9,115 +9,133 @@ LoRaModem modem;
 String appEui = "1322144234234235";
 String appKey = "A01DC8F9E363C86A883E41A6817427A5";
 
-#define DHTPIN 4
-#define DHTTYPE DHT11
-#define ONE_WIRE_BUS_1 10
-#define ONE_WIRE_BUS_2 9
-#define DOUT_PIN 13
-#define CLK_PIN 14
+int DHT_PIN = 4;
+int DHT_TYPE = DHT11;
+int ONE_WIRE_BUS_1 = 10;
+int ONE_WIRE_BUS_2 = 9;
+int DOUT_PIN = 13;
+int CLK_PIN = 14;
 
 OneWire oneWire1(ONE_WIRE_BUS_1);
 OneWire oneWire2(ONE_WIRE_BUS_2);
 DallasTemperature sensors1(&oneWire1);
 DallasTemperature sensors2(&oneWire2);
-DHT dht(DHTPIN, DHTTYPE);
-
+DHT dht(DHT_PIN, DHT_TYPE);
 HX711 scale;
 
-bool connected;
-int err_count;
-short con;
-float calibration_factor = 1.045;
-float units;
-float ounces;
+bool connected = false;
+int err_count = 0;
+short con = 0;
+const float CALIBRATION_FACTOR = 1.045;
 
 void setup() {
-  
-   Serial.begin(115200);
-   Serial.println("Carte MKRWAN n°11 connectée !\nTentative de connexion à l'application TTN...");
-   sensors1.begin(); // Init DS18B20 temp sensor n1
-   sensors2.begin(); // Init DS18B20 temp sensor n2
-   scale.begin(DOUT_PIN, CLK_PIN); // DT, CLK - Pins specified here
-   scale.set_scale();
-   scale.tare();
-   long zero_factor = scale.read_average();
-   scale.set_scale(calibration_factor);
-   
-   while (!Serial);
-   
-   Serial.println("Carte MKRWAN n°11 connectée !\nTentative de connexion à l'application TTN...");
-   modem.begin(EU868);
-   dht.begin();
-   delay(1000);      // apparently the murata dislike if this tempo is removed...
-   connected=false;
-   err_count=0;
-   con =0;
-   
+    initSerial();
+    initSensors();
+    initModem();
 }
 
 void loop() {
+    displayWeight();
+    displayTemperatures();
+    handleLoRaConnection();
+}
 
-   // Affichage de la température sur le moniteur série
-   Serial.print("[INFO] Poids mesuré : ");
-   units = scale.get_units(), 10;
-   if (units < 0) { units = 0.00; }
-   ounces = units * 0.035274;
-   Serial.print(ounces);
-   Serial.print(" grams"); 
-   Serial.println();
-   sensors1.requestTemperatures();
-   sensors2.requestTemperatures();
-   float ds18b20Temperature_1 = sensors1.getTempCByIndex(0);
-   float ds18b20Temperature_2 = sensors2.getTempCByIndex(0);
-   short dhtTemperature;
-   short dhtHumidity;
-   if ( !connected ) {
-      Serial.print("Join test : ");
-      Serial.println(++con);
-      int ret=modem.joinOTAA(appEui, appKey);
-    
-      if ( ret ) {
-         connected=true;
-         modem.minPollInterval(60);
-         Serial.println("Connected");
-         modem.dataRate(5);   // switch to SF7
-         delay(2000);          // because ... more stable -> 100
-         err_count=0;
-      }
-   }
+void initSerial() {
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.println("Carte MKRWAN n°11 connectée !\nTentative de connexion à l'application TTN...");
+}
 
-   if ( connected ) {
-      int err=0;
-      dhtTemperature = short(dht.readTemperature());
-      dhtHumidity = short(dht.readHumidity());
-      modem.beginPacket();
-      Serial.println("Temperature DHT11 = " + String(dhtTemperature) +" °C");
-      Serial.println("Humidite DHT11 = " + String(dhtHumidity) +" %");
-      Serial.println("Temperature DS18B20 (1) = " + String(short(ds18b20Temperature_1)) + " °C");
-      Serial.println("Temperature DS18B20 (2) = " + String(short(ds18b20Temperature_2)) + " °C");
-      modem.write(dhtTemperature);
-      modem.write(dhtHumidity);   
-      modem.write((short)ds18b20Temperature_1);
-      modem.write((short)ds18b20Temperature_2);
-      err = modem.endPacket();
-    
-      if ( err <= 0 ) {
-         Serial.print("Error : ");
-         Serial.println(err);
-         // Confirmation not received - jam or coverage fault
-         err_count++;
-         if ( err_count > 50 ) {
-            connected = false;
-         }
-         // wait for 2min for duty cycle with SF12 - 1.5s frame
-         for ( int i = 0 ; i < 120 ; i++ ) {
-            delay(1000);
-         }
-      } else {
-         err_count = 0;
-         // wait for 20s for duty cycle with SF7 - 55ms frame
-         delay(20000);
-         Serial.println("Message envoyé");   
-      }
-   }
+void initSensors() {
+    sensors1.begin();
+    sensors2.begin();
+    scale.begin(DOUT_PIN, CLK_PIN);
+    scale.set_scale();
+    scale.tare();
+    scale.set_scale(CALIBRATION_FACTOR);
+    dht.begin();
+}
+
+void initModem() {
+    modem.begin(EU868);
+    delay(1000);
+}
+
+void displayWeight() {
+    Serial.print("[INFO] ");
+    float weight_units = scale.get_units(10);
+    if (weight_units < 0) weight_units = 0.00;
+    long weight_grams = (long)(weight_units * 10); // Precision de 100g
+    Serial.println("Poids mesuré = " + String(weight_grams) + " grammes x 100 (en float : " + String(weight_units) + " g).");
+}
+
+void displayTemperatures() {
+    sensors1.requestTemperatures();
+    sensors2.requestTemperatures();
+    float ds18b20Temp1 = sensors1.getTempCByIndex(0);
+    float ds18b20Temp2 = sensors2.getTempCByIndex(0);
+    short dhtTemp = short(dht.readTemperature());
+    short dhtHumidity = short(dht.readHumidity());
+    Serial.println("[INFO] Temperature DHT11 = " + String(dhtTemp) + " °C");
+    Serial.println("[INFO] Humidite DHT11 = " + String(dhtHumidity) + " %");
+    Serial.println("[INFO] Temperature DS18B20 (1) = " + String(short(ds18b20Temp1)) + " °C");
+    Serial.println("[INFO] Temperature DS18B20 (2) = " + String(short(ds18b20Temp2)) + " °C");
+}
+
+void handleLoRaConnection() {
+    if (!connected) {
+        Serial.print("Join test : ");
+        Serial.println(++con);
+        int ret = modem.joinOTAA(appEui, appKey);
+        if (ret) {
+            connected = true;
+            modem.minPollInterval(60);
+            Serial.println("Connected");
+            modem.dataRate(5);
+            delay(2000);
+            err_count = 0;
+        }
+    }
+    if (connected) {
+        int err = sendLoRaPacket();
+        if (err <= 0) {
+            handleLoRaError(err);
+        } else {
+            err_count = 0;
+            delay(20000);
+            Serial.println("Message envoyé");
+        }
+    }
+}
+
+int sendLoRaPacket() {
+    float ds18b20Temp1 = sensors1.getTempCByIndex(0);
+    float ds18b20Temp2 = sensors2.getTempCByIndex(0);
+    short dhtTemp = short(dht.readTemperature());
+    short dhtHumidity = short(dht.readHumidity());
+    float weight_units = scale.get_units(10);
+    if (weight_units < 0) weight_units = 0.00;
+    long weight_grams = (long)(weight_units * 10); // 100g precision
+    modem.beginPacket();
+    modem.write(dhtTemp);
+    modem.write(dhtHumidity);
+    modem.write((short)ds18b20Temp1);
+    modem.write((short)ds18b20Temp2);
+    modem.write((weight_grams >> 24) & 0xFF);   // octet le plus significatif
+    modem.write((weight_grams >> 16) & 0xFF);   // deuxième octet le plus significatif
+    modem.write((weight_grams >> 8) & 0xFF);    // troisième octet le plus significatif
+    modem.write(weight_grams & 0xFF);           // octet le moins significatif
+    return modem.endPacket();
+}
+
+void handleLoRaError(int err) {
+    Serial.print("Error : ");
+    Serial.println(err);
+    err_count++;
+    if (err_count > 50) {
+        connected = false;
+    }
+    for (int i = 0; i < 120; i++) {
+        delay(1000);
+    }
 }
