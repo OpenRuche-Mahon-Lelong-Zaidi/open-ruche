cb39763d7e817ace86d348f59358e0d3be9709b7
gregoiremahon <greg.mahondu78@gmail.com>
2024-01-11 18:26:50 +0100

    Prise en compte des températures négatives

--- src/payload_formater_ttn.js
+++ src/payload_formater_ttn.js
@@ -1,17 +1,29 @@
 function decodeUplink(input) {
   var data = {};
-  data.luminosity = (input.bytes[1] << 8 | (input.bytes[0]));
-  data.temperatureDS18B20_1 = ((input.bytes[3] << 8 | (input.bytes[2])) / 100).toFixed(2);
-  data.temperatureDS18B20_2 = ((input.bytes[5] << 8 | (input.bytes[4])) / 100).toFixed(2);
-  data.weight = (input.bytes[7] << 8 | (input.bytes[6])) * 10;
-  data.humidityDHT22_2 = (input.bytes[9] << 8 | (input.bytes[8]));
-  data.humidityDHT22_3 = (input.bytes[11] << 8 | (input.bytes[10]));
-  data.temperatureDHT22_2 = ((input.bytes[13] << 8 | (input.bytes[12]))/ 100).toFixed(2);
-  data.temperatureDHT22_3 = ((input.bytes[15] << 8 | (input.bytes[14]))/ 100).toFixed(2);
-  data.batteryPercentage = (input.bytes[17] << 8 | (input.bytes[16]));
+  data.key = "3upgcl5ny1se8nva";
+  data.l = (input.bytes[1] << 8 | input.bytes[0]);
+  data.t_1 = decodeTemperature(input.bytes[2], input.bytes[3]);
+  data.t_2 = decodeTemperature(input.bytes[4], input.bytes[5]);
+  data.weight_kg = (((input.bytes[7] << 8 | input.bytes[6]) * 10) / 1000).toFixed(2);
+  data.h = ((input.bytes[9] << 8 | input.bytes[8]) / 100).toFixed(2); // exterieur
+  data.h_I = ((input.bytes[11] << 8 | input.bytes[10]) / 100).toFixed(2); // interieur
+  data.t = decodeTemperature(input.bytes[12], input.bytes[13]); // exterieur
+  data.t_0 = decodeTemperature(input.bytes[14], input.bytes[15]); // interieur
+  data.bv = ((input.bytes[17] << 8 | input.bytes[16]) / 100).toFixed(2);
+
   return {
-    data: data,
-    warnings: [],
-    errors: []
+      data: data,
+      warnings: [],
+      errors: []
   };
+}
+
+// Fonction pour décoder une température en tenant compte des valeurs signées
+function decodeTemperature(byte1, byte2) {
+  var temp = (byte2 << 8 | byte1);
+  // Vérifie si la température est négative
+  if (temp > 32767) {
+      temp = temp - 65536;
+  }
+  return (temp / 100).toFixed(2);
 }
